7a759ac35e48bab6dde4de68b8014b52b3aef66a
Sencerhan Taşar <sencerhan76@gmail.com>
2025-01-26 02:56:29 +0300

    bug fix

--- src/Commands/CreateMigrationsFromDatabaseCommand.php
+++ src/Commands/CreateMigrationsFromDatabaseCommand.php
@@ -1,1 +1,12 @@
-namespace Sencerhan\LaravelDbTools\Commands; 
+<?php
+
+namespace Sencerhan\LaravelDbTools\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CreateMigrationsFromDatabaseCommand extends Command
+{
+    // ... 
+} 
--- src/Commands/CreateSeederFromDatabaseCommand.php
+++ src/Commands/CreateSeederFromDatabaseCommand.php
@@ -1,1 +1,13 @@
-namespace Sencerhan\LaravelDbTools\Commands; 
+<?php
+
+namespace Sencerhan\LaravelDbTools\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
+class CreateSeederFromDatabaseCommand extends Command
+{
+    // ...
+} 
